760f7d996d8431e5e100901331794f77518f191e
Shabi Victor <38999931+shab0509@users.noreply.github.com>
2025-06-30 15:46:18 +0530

    Release: Updated Contents

--- Test_Scripts/xx4.py
+++ Test_Scripts/xx4.py
@@ -1,25 +1,22 @@
 from pyspark.sql import SparkSession
-from pyspark import SparkContext
-import findspark
-
-findspark.init()
+from pyspark import find_spark_home
+from spark import createDataFrame
 
 spark = SparkSession \
         .builder \
-        .appName("Filtering Nulls") \
+        .appName("Group+ Agg") \
         .getOrCreate()
 
-sc= spark.sparkContext
 
 
-line="              spark is for processing             "
-words=line.split(" ")
-print(words)
+r1= spark.read.text( "/Users/shabivictor/Shabi_Personal/GITHUB/Pyspark_Notes/Data_Files/emp.txt")
+r1.show()
+r2 = r1.map(lambda x: x.split(","))
+r2.show()
+#r3 = r2.map(lambda x: (x[4], int(x[2])))
+#r3.collect()
+#res = r3.reduceByKey(lambda x, y: x + y)
+#res.collect()
 
-words1 =sc.parallelize(words)
-'''
-words2=words1.filter(lambda x:x!='')
-words2.collect()
-y=words2.collect()
-z=" ".join(y)
-print(z)'''
+
+
