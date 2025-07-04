1dd5c433f8b7379d138bc16d0f8aa8febe0e8928
Shabi Victor <38999931+shab0509@users.noreply.github.com>
2025-07-04 13:53:00 +0530

    Release : Updated Contents

--- Test_Scripts/xx1.py
+++ Test_Scripts/xx1.py
@@ -1,12 +1,18 @@
 from pyspark import core, RDD
 from pyspark.sql import SparkSession
-from importlib.resources import files
 
 # Create a SparkSession
-sc = SparkSession.builder \
-    .appName("BasicPySparkApp") \
-    .config("spark.driver.bindAddress", "localhost") \
-    .getOrCreate()
+'''
+spark = SparkSession.builder \
+        .appName("BasicPySparkApp") \
+        .config("spark.driver.bindAddress", "localhost") \
+        .getOrCreate()
+'''
+spark = SparkSession.builder \
+        .appName("BasicPySparkApp") \
+        .getOrCreate()
+
+sc =spark.sparkContext
 
 data1=[10,20,30,40,50]
 data2=[10,20,30,60,70]
@@ -17,8 +23,8 @@ print(data2)
 print(type(data2))
 
 RDD1 =sc.parallelize(data1)
-RDD2 =sc.parallelize(data2)
-print(RDD1)
+RDD2 =spark.createDataFrame(data2)
+print(RDD1.show())
 print(type(RDD1))
-print(RDD2)
+print(RDD2.show())
 print(type(RDD2))
